b6141ce554b2e5c764bd2e5de44d1c11bedb1653
Oleksii Plaunov <plaunov@gmail.com>
2026-05-01 13:20:30 +0300

    Settings test and POM added

---
+++ pages/SettingsPage.ts
@@ -0,0 +1,49 @@
+// https://demo.learnwebdriverio.com/settings
+
+import { type Locator, type Page, expect } from "@playwright/test";
+
+export class SettingsPage {
+  readonly page: Page;
+  readonly yourSettingsHeading: Locator;
+  readonly signInLink: Locator;
+  readonly signUpLink: Locator;
+  readonly userNameProfileLink: Locator;
+  readonly settingsPageLink: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+
+    // Define Locators
+    this.yourSettingsHeading = page.getByRole("heading", {
+      name: "Your Settings",
+    });
+    this.signInLink = this.page
+      .getByRole("listitem")
+      .filter({ hasText: "Sign in" });
+    this.signUpLink = this.page
+      .getByRole("listitem")
+      .filter({ hasText: "Sign up" });
+    this.userNameProfileLink = this.page.getByRole("link", {
+      name: process.env.USER_NAME,
+    });
+    this.settingsPageLink = this.page
+      .getByRole("listitem")
+      .filter({ hasText: "Settings" });
+  }
+
+  async goto() {
+    await this.settingsPageLink.click();
+  }
+
+  async verifyUserIsSignedIn() {
+    await expect(this.signInLink).not.toBeVisible();
+    await expect(this.signUpLink).not.toBeVisible();
+    await expect(this.userNameProfileLink).toBeVisible();
+  }
+
+  async verifyUserIsNotSignedIn() {
+    await expect(this.signInLink).toBeEnabled();
+    await expect(this.signUpLink).toBeEnabled();
+    await expect(this.userNameProfileLink).not.toBeVisible();
+  }
+}
