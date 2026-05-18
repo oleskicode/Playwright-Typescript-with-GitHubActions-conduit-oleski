7702c5b52aef2675b232996d20c42ea9a8131651
Oleksii Plaunov <plaunov@gmail.com>
2026-05-18 18:25:28 +0300

    API Parametrization for Article Validation

---
+++ tests/API_tests/api.articles.validation.parametrized.spec.ts
@@ -0,0 +1,63 @@
+import { test, expect } from "../../fixtures/auth.api.fixture";
+import { ArticleBuilder } from "../../helpers/articleBuilder";
+
+let articleSlug: string | undefined;
+
+test.afterEach(async ({ request, authToken }) => {
+  if (!articleSlug) return;
+
+  const response = await request.delete(
+    `${process.env.API_BASE_URL}/articles/${articleSlug}`,
+    {
+      headers: {
+        Authorization: `Token ${authToken}`,
+      },
+    },
+  );
+
+  expect(response.ok()).toBeTruthy();
+  articleSlug = undefined;
+});
+
+const testCases = [
+  {
+    testCase: "empty title",
+    buildArticle: () => new ArticleBuilder().withTitle("").build(),
+  },
+  {
+    testCase: "empty body",
+    buildArticle: () => new ArticleBuilder().withBody("").build(),
+  },
+  {
+    testCase: "empty body and title",
+    buildArticle: () => new ArticleBuilder().withTitle("").withBody("").build(),
+  },
+];
+
+for (const { testCase: testCase, buildArticle } of testCases) {
+  test(`API - create article with ${testCase}`, async ({
+    request,
+    authToken,
+  }) => {
+    const authHeaders = {
+      Authorization: `Token ${authToken}`,
+      "Content-Type": "application/json",
+    };
+
+    const invalidArticle = buildArticle();
+
+    const response = await request.post(
+      `${process.env.API_BASE_URL}/articles`,
+      {
+        headers: authHeaders,
+        data: invalidArticle,
+      },
+    );
+
+    const responseBody = await response.json();
+    articleSlug = responseBody.article.slug;
+
+    expect(response.ok()).toBeTruthy();
+    expect(response.status()).toBe(200);
+  });
+}
