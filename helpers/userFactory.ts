b8ad7f120b73179333d2cd5aaf6cf610aed9612a
Oleksii Plaunov <plaunov@gmail.com>
2026-06-26 23:57:09 +0300

    userFactory - improved test data - collision risk lowered

--- helpers/userFactory.ts
+++ helpers/userFactory.ts
@@ -8,9 +8,12 @@ export interface User {
 }
 
 export const createUser = (overrides?: Partial<User>): User => {
+  // Append a unique suffix to guarantee no clash in test data
+  const uniqueSuffix = `${Date.now()}${Math.floor(Math.random() * 100000)}`;
+
   return {
-    username: faker.person.firstName(),
-    email: faker.internet.email(),
+    username: `${faker.person.firstName()}${uniqueSuffix}`,
+    email: `${uniqueSuffix}.${faker.internet.email()}`,
     password: faker.internet.password({ length: 12 }),
     bio: faker.person.bio(),
     ...overrides,
